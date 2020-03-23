862a233f3da9ada1b6923b3478a992a8488ecf42
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-03-23 15:52:58 +0800

    Fix database error

--- registration.php
+++ registration.php
@@ -54,8 +54,8 @@
                 
                 if(valid){
 
-                var firstname   = $('#firstname').val();
-                var lastname    = $('#lastname').val();
+                var firstname   = $('#fn').val();
+                var lastname    = $('#ln').val();
                 var email       = $('#email').val();
                 var phone       = $('#phone').val();
                 var pass        = $('#pass').val();
