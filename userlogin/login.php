84245f6f3c53e84975f5cefb1c82237cbdf7a586
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-03-24 16:22:04 +0800

    login System
    there is an error in css.

---
+++ userlogin/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="bootstrap/bootstrapCDN.css">
+    <link rel="stylesheet" href="fontAwesomeCDN/fontAwesomeCDN.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Project</title>
+</head>
+<body>
+<div class="container h-100">
+    <div class="d-flex justify-content-center h-100">
+        <div class="user_card">
+            <div class="d-flex justify-content-center">
+                <div class="brand_logo_container">
+                    <img src="img/login_img.png" class="brand_logo" alt="Project Logo">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+    
+    <script
+        src="JQeuryCDN/jqueryCDN.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+        crossorigin="anonymous">
+    </script>
+    <script src="BootstrapCDN/BootstrapCDN.js"></script>
+</body>
+</html>
+
