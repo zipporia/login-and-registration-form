b3e8ff80d636c4112eb34bd0433ed3edabe54123
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-04-04 23:06:57 +0800

    function for login jquery fix

--- userlogin/login.php
+++ userlogin/login.php
@@ -53,11 +53,10 @@
     </div>
 </div>
     
-    <script
-        src="JQeuryCDN/jqueryCDN.js"
-        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-        crossorigin="anonymous">
-    </script>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
     <script src="BootstrapCDN/BootstrapCDN.js"></script>
     <script src='css/fontawesome.js'></script>
     <script>
