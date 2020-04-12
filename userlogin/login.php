48993c399dcd6b72fd2c55cbb042fa44868c4ab4
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-04-12 20:45:11 +0800

    user login is set

--- userlogin/login.php
+++ userlogin/login.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    if(isset($_SESSION['userlogin'])){
+        header("Location: main.php");
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
--- userlogin/main.php
+++ userlogin/main.php
@@ -1,2 +1,10 @@
 <?php
+    session_start();
+    if(!isset($_SESSION['userlogin'])){
+        header("Location: login.php");
+    }
     echo "this is main";
+?>
+    
+
+<p>Welcom to Main</p>
