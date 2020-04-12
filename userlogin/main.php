deb90cf408a66a619fcdc932bda1b2fa95468342
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-04-12 20:48:32 +0800

    Logout link set

--- userlogin/main.php
+++ userlogin/main.php
@@ -4,7 +4,15 @@
         header("Location: login.php");
     }
     echo "this is main";
+
+    if(isset($_GET['logout'])){
+        session_destroy();
+        unset($_SESSION);
+        header("Location: login.php");
+    }
 ?>
     
 
 <p>Welcom to Main</p>
+
+<a href="main.php?logout=true">Logout</a>
