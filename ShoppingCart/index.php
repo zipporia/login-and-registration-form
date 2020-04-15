462a76b5877b4c607eadc4f7774ea4c2b1e3eb0e
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-04-15 23:40:22 +0800

    added keyboard product

--- ShoppingCart/index.php
+++ ShoppingCart/index.php
@@ -70,7 +70,7 @@
                 </form>
             </div>
             <div class="col-md-3 col-sm-6 my-3 my-md-0">
-            <form action="index.php" method="post">
+                <form action="index.php" method="post">
                     <div class="card shadow">
                         <div>
                             <img src="./image/monitor.png" alt="image1" class="img-fluid card-img-top">
@@ -97,7 +97,31 @@
                 </form>
             </div>
             <div class="col-md-3 col-sm-6 my-3 my-md-0">
-            
+            <form action="index.php" method="post">
+                    <div class="card shadow">
+                        <div>
+                            <img src="./image/keyboard.png" alt="image1" class="img-fluid card-img-top">
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">Keyboard</h5>
+                            <h6>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="far fa-star"></i>
+                            </h6>
+                            <p class="card-text">
+                                text to build on the card
+                            </p>
+                            <h5>
+                                <small><s class="text-secondary">Php 1399.75</s></small>
+                                <span class="price">Php 1199.75</span>
+                            </h5>
+                            <button type="submit" class="btn btn-warning my-3" name="add">Add to Cart <i class="fas fa-shopping-cart"></i></button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
