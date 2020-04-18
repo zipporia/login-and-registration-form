3b7256fcc8efe773270b4d2e264c559b069f6615
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-04-18 22:37:01 +0800

    problem on component.php

--- ShoppingCart/index.php
+++ ShoppingCart/index.php
@@ -1,3 +1,7 @@
+<?php
+    require_once 'php/component.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,39 +19,11 @@
 <body>
     <div class="container">
         <div class="row text-center py-5">
-            <div class="col-md-3 col-sm-6 my-3 my-md-0">
-                <form action="index.php" method="post">
-                    <div class="card shadow">
-                        <div>
-                            <img src="./image/headset.png" alt="image1" class="img-fluid card-img-top">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Head set</h5>
-                            <h6>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="far fa-star"></i>
-                            </h6>
-                            <p class="card-text">
-                                text to build on the card
-                            </p>
-                            <h5>
-                                <small><s class="text-secondary">Php 999.75</s></small>
-                                <span class="price">Php 799.75</span>
-                            </h5>
-                            <button type="submit" class="btn btn-warning my-3" name="add">Add to Cart <i class="fas fa-shopping-cart"></i></button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            <div class="col-md-3 col-sm-6 my-3 my-md-0">
-            </div>
-            <div class="col-md-3 col-sm-6 my-3 my-md-0">
-            </div>
-            <div class="col-md-3 col-sm-6 my-3 my-md-0">
-            </div>
+            <?php
+            component();
+            component();
+            component();
+            ?>
         </div>
     </div>
 
--- ShoppingCart/php/component.php
+++ ShoppingCart/php/component.php
@@ -30,4 +30,5 @@ function component(){
         </form>
     </div>
     ";
+    echo element;
 }
