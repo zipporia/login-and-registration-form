6fa814214e6cddb9015c8fd440f98a635d373002
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-04-18 22:33:22 +0800

    delete html product

--- ShoppingCart/index.php
+++ ShoppingCart/index.php
@@ -43,85 +43,10 @@
                 </form>
             </div>
             <div class="col-md-3 col-sm-6 my-3 my-md-0">
-                <form action="index.php" method="post">
-                    <div class="card shadow">
-                        <div>
-                            <img src="./image/mouse.png" alt="image1" class="img-fluid card-img-top">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Mouse</h5>
-                            <h6>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="far fa-star"></i>
-                            </h6>
-                            <p class="card-text">
-                                text to build on the card
-                            </p>
-                            <h5>
-                                <small><s class="text-secondary">Php 1899.75</s></small>
-                                <span class="price">Php 1399.75</span>
-                            </h5>
-                            <button type="submit" class="btn btn-warning my-3" name="add">Add to Cart <i class="fas fa-shopping-cart"></i></button>
-                        </div>
-                    </div>
-                </form>
             </div>
             <div class="col-md-3 col-sm-6 my-3 my-md-0">
-                <form action="index.php" method="post">
-                    <div class="card shadow">
-                        <div>
-                            <img src="./image/monitor.png" alt="image1" class="img-fluid card-img-top">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Monitor</h5>
-                            <h6>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="far fa-star"></i>
-                            </h6>
-                            <p class="card-text">
-                                text to build on the card
-                            </p>
-                            <h5>
-                                <small><s class="text-secondary">Php 6899.75</s></small>
-                                <span class="price">Php 4399.75</span>
-                            </h5>
-                            <button type="submit" class="btn btn-warning my-3" name="add">Add to Cart <i class="fas fa-shopping-cart"></i></button>
-                        </div>
-                    </div>
-                </form>
             </div>
             <div class="col-md-3 col-sm-6 my-3 my-md-0">
-            <form action="index.php" method="post">
-                    <div class="card shadow">
-                        <div>
-                            <img src="./image/keyboard.png" alt="image1" class="img-fluid card-img-top">
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Keyboard</h5>
-                            <h6>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="fas fa-star"></i>
-                                <i class="far fa-star"></i>
-                            </h6>
-                            <p class="card-text">
-                                text to build on the card
-                            </p>
-                            <h5>
-                                <small><s class="text-secondary">Php 1399.75</s></small>
-                                <span class="price">Php 1199.75</span>
-                            </h5>
-                            <button type="submit" class="btn btn-warning my-3" name="add">Add to Cart <i class="fas fa-shopping-cart"></i></button>
-                        </div>
-                    </div>
-                </form>
             </div>
         </div>
     </div>
