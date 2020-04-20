7e03120a7a2ffb6d0490dbbf3e15c9d4b910a2e9
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-04-20 22:56:01 +0800

    mouse product added

--- ShoppingCart/index.php
+++ ShoppingCart/index.php
@@ -23,6 +23,7 @@
                 component("Head Set", 1599.75, "./image/headset.png", 1899.75);
                 component("keyboard", 1199.75, "./image/keyboard.png", 1599.75);
                 component("Monitor", 3299.75, "./image/monitor.png", 4899.75);
+                component("Mouse", 1299.75, "./image/mouse.png", 1799.75);
             ?>
         </div>
     </div>
