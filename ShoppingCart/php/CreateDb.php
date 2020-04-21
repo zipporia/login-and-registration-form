b479880318287353b3e430fb053c06740f5ec973
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-04-21 22:45:49 +0800

    create database

--- ShoppingCart/php/CreateDb.php
+++ ShoppingCart/php/CreateDb.php
@@ -43,6 +43,14 @@ class CreateDb{
         if(mysqli_query($this->con, $sql)){
 
             $this->con = mysqli_connect($servername, $username, $password, $dbname);
+        
+            $sql = "CREATE TABLE IF NOT EXISTS $tablename
+                    (id INT(11)NOT NULL AUTO_INCREMENT PRIMARY KEY
+                    product_name VARCHAR(25)NOT NULL,
+                    product_price FLOAT,
+                    product_image VARCHAR(100)
+                    );";
+        
         }
 
     }
