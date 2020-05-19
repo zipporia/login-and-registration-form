e22e1fe088a4c0b10b88f2cff9cfd3f65df20cb7
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2020-05-19 14:26:55 +0800

    changed product name and price

--- ShoppingCart/php/inserted product.sql
+++ ShoppingCart/php/inserted product.sql
@@ -1,5 +1,5 @@
 INSERT INTO producttb (product_name, product_price, product_image) VALUES 
-("Head 1", 1599.75, "./image/headset.png"),
-("keyboard 2", 1000.75, "./image/keyboard.png"),
+("Head set 1", 1599.75, "./image/headset.png"),
+("keyboard 2", 1999.75, "./image/keyboard.png"),
 ("Monitor 3", 3599.75, "./image/monitor.png"),
 ("mouse 4", 1299.75, "./image/mouse.png")
